d4694833a5d5d6ada250a08cb53058105ad2f16c
ben447068-gif <ben447068@gmail.com>
2025-08-30 07:13:44 +0100

    Add files via upload
    File deliver complete

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "social-app-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// Install service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch from cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
